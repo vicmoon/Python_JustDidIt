d8b2e30a6a93e7dd7d2195f29c222982eefa0497
Victoria Munteanu <munteanuvictoria1@gmail.com>
2025-10-02 20:26:25 +0200

    update footer and forms

--- forms.py
+++ forms.py
@@ -10,16 +10,6 @@ class ActivityForm(FlaskForm):
     icon_ref = StringField(validators=[Optional()])    
     submit = SubmitField("Do It!")
 
-    # Ensure at least one is chosen
-    # def validate(self, *a, **kw):
-    #     ok = super().validate(*a, **kw)
-    #     if not ok:
-    #         return False
-    #     if not (self.icon.data or self.icon_ref.data):
-    #         self.icon_ref.errors.append("Pick an icon (from list or search).")
-    #         return False
-    #     return True
-
 class RegisterForm(FlaskForm):
 
     name= StringField('Name', validators=[DataRequired()],render_kw={"placeholder": "Name"})
